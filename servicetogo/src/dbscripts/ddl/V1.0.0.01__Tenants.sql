bfa76d845f42e3963c132af46dfb9cc4d4cf26fc
Ranjana <ranjanas20@gmail.com>
2018-06-10 12:02:53 -0700

    Added audit fields

--- servicetogo/src/dbscripts/ddl/V1.0.0.01__Tenants.sql
+++ servicetogo/src/dbscripts/ddl/V1.0.0.01__Tenants.sql
@@ -1,18 +1,22 @@
--- Table: public.tenants
+-- Table: public.tenant
 
--- DROP TABLE public.tenants;
+-- DROP TABLE public.tenant;
 
-CREATE TABLE public.tenants
+CREATE TABLE public.tenant
 (
     tenant_id character varying(10) COLLATE pg_catalog."default" NOT NULL,
     tenant_name character varying(20) COLLATE pg_catalog."default" NOT NULL,
     tenant_status character varying(10) COLLATE pg_catalog."default" NOT NULL,
-    CONSTRAINT "Tenants_pkey" PRIMARY KEY (tenant_id)
+    created_on timestamp ,
+    created_by character varying(20) COLLATE pg_catalog."default",
+    updated_on timestamp,
+    updated_by character varying(20) COLLATE pg_catalog."default",
+    CONSTRAINT "Tenant_pkey" PRIMARY KEY (tenant_id)
 )
 WITH (
     OIDS = FALSE
 )
 TABLESPACE pg_default;
 
-ALTER TABLE public.tenants
+ALTER TABLE public.tenant
     OWNER to postgres;
