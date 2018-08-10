172437e79b56c765ca69e2925b7530f928b5a22c
Ranjana <ranjanas20@gmail.com>
2018-08-10 07:38:33 -0700

    some code table data

---
+++ servicetogo/src/dbscripts/dml/V1.0.0.19__code_table_data_insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO public.code_table
+(code_group, code_id, code_name)
+VALUES('VehicleLocation', 'Home', 'Home'), 
+('VehicleLocation', 'Office', 'Office'), 
+('VehicleLocation', 'Others', 'Others');
+
+INSERT INTO public.code_table
+(code_group, code_id, code_name)
+VALUES('ContactMethod', 'Email', 'Email'), 
+('ContactMethod', 'Phone', 'Phone'), 
+('ContactMethod', 'Text', 'Text');
